d5873075d7621c362f739afb5a60b745922f46ab
twkim991 <darkbell991@gmail.com>
2025-06-22 11:59:39 +0900

    오타 수정

--- crawling.js
+++ crawling.js
@@ -617,8 +617,8 @@ function parseExcel(Platform, yesterday) {
 				let name = '';
 				let totalSalesCount = 0;
 				let totalRevenue = 0;
-				// if(idx < 2 || row[0] == '조회한 결과가 없습니다.' || row[0] != yesterday) {return;}
-				if(idx < 2) {return;}
+				if(idx < 2 || row[0] == '조회한 결과가 없습니다.' || row[0] != yesterday) {return;}
+				// if(idx < 2) {return;}
 				content_no = data2[row[1]];
 				totalSalesCount = row[3];
 				totalRevenue = row[4];
@@ -1407,17 +1407,17 @@ async function downloadbookcube() {
 async function crawling(platform) {
 	const salesDate = getYesterday('file');
 	let data = [];
-	if(platform=="series") {await downloadseries();}
-	else if(platform=="kakao") {await downloadkakao();}
-	else if(platform=="ridi") {await downloadridi();}
-	else if(platform=="kyobo") {await downloadkyobo();}
-	else if(platform=="aladin") {await downloadaladin();}
-	else if(platform=="joara") {data = await downloadjoara();}
-	else if(platform=="blice") {await downloadblice();}
-	// else if(platform=="piuri") {await downloadpiuri();};
-	else if(platform=="yes24") {await downloadyes24();}
-	else if(platform=="bomtoon") {await downloadbomtoon();}
-	else if(platform=="bookcube") {await downloadbookcube();}
+	// if(platform=="series") {await downloadseries();}
+	// else if(platform=="kakao") {await downloadkakao();}
+	// else if(platform=="ridi") {await downloadridi();}
+	// else if(platform=="kyobo") {await downloadkyobo();}
+	// else if(platform=="aladin") {await downloadaladin();}
+	// else if(platform=="joara") {data = await downloadjoara();}
+	// else if(platform=="blice") {await downloadblice();}
+	// // else if(platform=="piuri") {await downloadpiuri();};
+	// else if(platform=="yes24") {await downloadyes24();}
+	// else if(platform=="bomtoon") {await downloadbomtoon();}
+	// else if(platform=="bookcube") {await downloadbookcube();}
 
 	await sleep(1000);
 	if (platform !== "joara") {
@@ -1427,9 +1427,9 @@ async function crawling(platform) {
 
 	console.log(data)
 
-	for(row of data) {
-		await saveToDB(row[0], row[1], platform, row[2], row[3], row[4], salesDate);
-	}
+	// for(row of data) {
+	// 	await saveToDB(row[0], row[1], platform, row[2], row[3], row[4], salesDate);
+	// }
 }
 
 // const run = async () => {
@@ -1442,15 +1442,15 @@ async function crawling(platform) {
 // }
 
 const run = async () => {
-	await crawling("series");
-	await crawling("kakao");
-	await crawling("ridi");
-	await crawling("kyobo");
-	await crawling("aladin");
-	await crawling("joara");
-	await crawling("blice");
-	await crawling("yes24");
-	await crawling("bomtoon");
+	// await crawling("series");
+	// await crawling("kakao");
+	// await crawling("ridi");
+	// await crawling("kyobo");
+	// await crawling("aladin");
+	// await crawling("joara");
+	// await crawling("blice");
+	// await crawling("yes24");
+	// await crawling("bomtoon");
 	await crawling("bookcube");
 	console.log('✅ 모든 플랫폼 크롤링 및 저장 완료!');
   	process.exit(0);  // 👈 Node.js 프로세스 종료
